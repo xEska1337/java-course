aa1cf1c84cb7fbe99f5d10e3fdb86cca12450c52
kurs <szymonkaczmarek030@gmail.com>
2021-10-28 16:59:46 +0200

    First commit

---
+++ src/main/java/pl/teslachorzow/afirstproject/Main01.java
@@ -0,0 +1,9 @@
+package pl.teslachorzow.afirstproject;
+
+// ctrl+alt+l code alignment
+
+public class Main01 {
+    public static void main(String[] args) {        //make with shortcut psvm
+        System.out.println("Hej");                  //make with shortcut sout
+    }
+}
