9f5884941777d6e9a976ed077fba6de3c6f167fd
Szymon Kaczmarek <szymonkaczmarek54@gmail.com>
2022-01-20 15:55:29 +0100

    files

--- src/main/java/pl/teslachorzow/afirstproject/lproject/Main03.java
+++ src/main/java/pl/teslachorzow/afirstproject/lproject/Main03.java
@@ -2,24 +2,41 @@ package pl.teslachorzow.afirstproject.lproject;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main03 {
     public static void main(String[] args) {
         File file = new File("text3.txt");
-        String scan = " ";
-        int suma = 0;
+        StringBuilder reading = new StringBuilder();
+        double s = 0;
         try {
             Scanner scanner = new Scanner(file);
-            while (scanner.hasNext()){
-                System.out.println(scanner.next().replace(",",""));
-                //scan = scanner.next().replace(",","");
-
-                //suma += scanner.nextDouble();
+            while (scanner.hasNextLine()){
+                reading.append(scanner.nextLine() + "\n");
             }
+            String string = reading.toString();
+            String[] parts = string.split(",");
+            for (int i = 0; i < parts.length; i++) {
+                try {
+                    double partDouble = Double.parseDouble(parts[i]);
+                    s += partDouble;
+                } catch (NumberFormatException e) {
+                    System.out.println("error");
+                }
+            }
+            System.out.println("suma: " + s);
+            System.out.println(Arrays.toString(parts));
         } catch (FileNotFoundException ex){
             System.out.println("Brak pliku");
         }
 
     }
 }
+
+
+
+
+
+
+
